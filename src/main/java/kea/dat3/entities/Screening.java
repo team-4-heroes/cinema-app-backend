ba3bf0a43317ccf28a2b0dbc4a19ac71092477a1
ptsand <ptrsand@gmail.com>
2022-05-13 13:28:39 +0200

    added some testdata

--- src/main/java/kea/dat3/entities/Screening.java
+++ src/main/java/kea/dat3/entities/Screening.java
@@ -1,5 +1,6 @@
 package kea.dat3.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import kea.dat3.dto.ScreeningRequest;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,7 +22,10 @@ public class Screening {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
+    // "2015-03-25T12:00:00Z"
+    //@JsonFormat(pattern = "dd-MM HH:mm",shape = JsonFormat.Shape.STRING)
+    //@JsonFormat(shape = JsonFormat.Shape.NUMBER_INT)
+    //@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ",shape = JsonFormat.Shape.STRING)
     private LocalDateTime startTime;
 
     @ManyToOne
