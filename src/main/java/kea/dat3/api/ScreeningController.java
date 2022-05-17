c63d47bb24e46b3e69cd5152f5761d780277a99a
ptsand <ptrsand@gmail.com>
2022-05-17 10:26:09 +0200

    create and delete screening requires admin role

--- src/main/java/kea/dat3/api/ScreeningController.java
+++ src/main/java/kea/dat3/api/ScreeningController.java
@@ -6,6 +6,7 @@ import kea.dat3.services.ScreeningService;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
 @RestController
@@ -18,7 +19,7 @@ public class ScreeningController {
         this.screeningService = screeningService;
     }
 
-    // @RolesAllowed("ADMIN") todo: uncomment and fix test
+    @RolesAllowed("ADMIN")
     @PostMapping
     public ScreeningResponse addScreening(@RequestBody ScreeningRequest screeningRequest) {
         return screeningService.addScreening(screeningRequest);
@@ -39,6 +40,7 @@ public class ScreeningController {
         return screeningService.updateScreening(id, screeningRequest);
     } */
 
+    @RolesAllowed("ADMIN")
     @DeleteMapping("/{id}")
     public void deleteScreening(@PathVariable long id) {
         screeningService.deleteScreening(id);
--- src/test/java/kea/dat3/api/ScreeningControllerTest.java
+++ src/test/java/kea/dat3/api/ScreeningControllerTest.java
@@ -2,12 +2,13 @@ package kea.dat3.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import kea.dat3.dto.ScreeningRequest;
-import kea.dat3.entities.Movie;
-import kea.dat3.entities.Room;
-import kea.dat3.entities.Screening;
+import kea.dat3.entities.*;
 import kea.dat3.repositories.MovieRepository;
+import kea.dat3.repositories.PersonRepository;
 import kea.dat3.repositories.RoomRepository;
 import kea.dat3.repositories.ScreeningRepository;
+import kea.dat3.security.dto.LoginRequest;
+import kea.dat3.security.dto.LoginResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -18,19 +19,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@AutoConfigureMockMvc //(addFilters = false)
+@AutoConfigureMockMvc
 public class ScreeningControllerTest {
 
     @Autowired
@@ -43,6 +44,8 @@ public class ScreeningControllerTest {
     RoomRepository roomRepository;
     @Autowired
     ObjectMapper objectMapper;
+    @Autowired
+    PersonRepository personRepository;
 
     Screening s;
     long nonExistentId = 999;
@@ -52,22 +55,27 @@ public class ScreeningControllerTest {
         movieRepository.deleteAll();
         screeningRepository.deleteAll();
         roomRepository.deleteAll();
+        personRepository.deleteAll();
+        Person admin = new Person("email@testAdmin.dk", "gitteAdmin", "test", "22342122", "admin", "password");
+        admin.addRole(Role.ADMIN);
+        personRepository.save(admin);
         LocalDateTime startTime = LocalDateTime.of(2022, 10, 1, 10, 0);
         Room room = roomRepository.save(new Room("testRoom"));
         Movie movie = movieRepository.save(new Movie("film titel", "beskrivelse", 2000, 100, 100));
         s = new Screening(startTime, room, movie);
         screeningRepository.save(s);
-        //System.out.println(screeningRepository.count() +"Screenings:"+ screeningRepository.findAll());
     }
 
     @ParameterizedTest
     @ValueSource(ints = {-130, 200}) // runs once with each value as the minutes parameter
     public void testAddScreening(int minutes) throws Exception {
+        String jwt = login("admin","password");
         ScreeningRequest screeningReq = new ScreeningRequest(s.getStartTime().plusMinutes(minutes), s.getRoom().getId(), s.getMovie().getId());
         // Check that server answer with msg and statusCode
         mockMvc.perform(MockMvcRequestBuilders.post("/api/screenings")
                         .contentType("application/json")
                         .accept("application/json")
+                        .header("Authorization","Bearer " + jwt)
                         .content(objectMapper.writeValueAsString(screeningReq)))
                 .andDo(print())
                 .andExpect(status().isOk())
@@ -88,8 +96,8 @@ public class ScreeningControllerTest {
 
     @Test
     public void testAddScreeningWhenRoomNonexistent() throws Exception {
-        // Room room = new Room("notSavedRoom");
-        ScreeningRequest screeningReq = new ScreeningRequest(s.getStartTime().plusMinutes(200), 0, s.getMovie().getId());
+        long roomId = 0;
+        ScreeningRequest screeningReq = new ScreeningRequest(s.getStartTime().plusMinutes(200), roomId, s.getMovie().getId());
         // Check that server answer with msg and statusCode
         addScreeningFail(screeningReq, "Room with id '0' not found", "404");
         // Verify only one screening actually ended in the database
@@ -111,9 +119,11 @@ public class ScreeningControllerTest {
 
     @Test
     public void testDeleteNonexistentScreening() throws Exception {
+        String jwt = login("admin","password");
         mockMvc.perform(MockMvcRequestBuilders
                         .delete("/api/screenings/" + nonExistentId)
-                        .accept(MediaType.APPLICATION_JSON))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .header("Authorization","Bearer " + jwt))
                 .andDo(print())
                 .andExpect(status().isNotFound())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.path").exists())
@@ -123,17 +133,49 @@ public class ScreeningControllerTest {
         assertEquals(1, screeningRepository.count());
     }
 
+    @Test
+    public void testDeleteScreeningAsCustomer() throws Exception {
+        Person customer = new Person("email@testUser.dk", "gitteUser", "test", "12342121", "customer", "password");
+        customer.addRole(Role.CUSTUMER);
+        personRepository.save(customer);
+        String jwt = login("customer","password");
+        mockMvc.perform(MockMvcRequestBuilders
+                        .delete("/api/screenings/" + nonExistentId)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .header("Authorization","Bearer " + jwt))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+                //.andExpect(MockMvcResultMatchers.jsonPath("$.path").exists())
+                //.andExpect(MockMvcResultMatchers.jsonPath("$.error").value("Screening with id '" + nonExistentId + "' not found"))
+                //.andExpect(MockMvcResultMatchers.jsonPath("$.status").value("404"));
+
+        assertEquals(1, screeningRepository.count());
+    }
+
     private void addScreeningFail(ScreeningRequest screeningRequest, String msg, String statusCode) throws Exception {
+        String jwt = login("admin","password");
         mockMvc.perform(MockMvcRequestBuilders.post("/api/screenings")
                         .contentType("application/json")
                         .accept("application/json")
+                        .header("Authorization","Bearer " + jwt)
                         .content(objectMapper.writeValueAsString(screeningRequest)))
                 .andDo(print())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.path").exists())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.error").value(msg))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.status").value(statusCode));
     }
-    // TODO: create loginAsAdmin to return a token for creating screenings
+    // We need a token to add or delete screenings
+    private String login(String username, String pw) throws Exception {
+        MvcResult response = mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/auth/login")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(new LoginRequest(username, pw))))
+                .andReturn();
+        if (response.getResponse().getStatus() == 401) {
+            throw new Exception("Failed to login");
+        }
+        return objectMapper.readValue(response.getResponse().getContentAsString(), LoginResponse.class).getToken();
+    }
 }
 
 
